fc894763b6e393d294e1924ae33dd811f620d81d
Lawliet183 <frankharkin9181@gmail.com>
2026-05-28 08:12:47 -0600

    Mover cadena de conexion de MySQL a appsettings.json

--- ControlVentas.API/Models/VentasDbContext.cs
+++ ControlVentas.API/Models/VentasDbContext.cs
@@ -54,10 +54,6 @@ public partial class VentasDbContext : DbContext
 
     public virtual DbSet<Venta> Ventas { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=control_ventas_db;uid=root;pwd=admin", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.45-mysql"));
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
